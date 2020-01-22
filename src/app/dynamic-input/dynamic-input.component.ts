cf230593f8c397b5f3ede64343dc1a376e8c5200
Rakesh-Jatwa <rakeshjatwa72@gmail.com>
2020-01-22 20:25:48 +0530

    22dt task complete for dynamic forms creation

--- src/app/dynamic-input/dynamic-input.component.ts
+++ src/app/dynamic-input/dynamic-input.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { isNumber } from 'util';
 
 @Component({
   selector: 'app-dynamic-input',
@@ -10,63 +9,83 @@ import { isNumber } from 'util';
 export class DynamicInputComponent implements OnInit {
   public isSubmitted = false;
   registrationForm = this.fb.group({
-    gender: ['', [Validators.required]],
+    radios: ['', [Validators.required]],
     fields: ['', [Validators.required]],
     labels: ['', [Validators.required]]
-      })
-  InpuValue1: number;
-  itemList: any = [];
-  fields:any = [];
-  InpuValue2: any;
-  StoreValue: any;
-  Message: string;
-
-
-  constructor(private fb:FormBuilder) { }
+  })
+  public InpuValue1: number;
+  public  itemList: any = [];
+  public fields: any = [];
+  public InpuValue2: any;
+  public arrdataIndex: any;
+  public arrdata: any;
+  public SaveButton: boolean = false;
+  public EShowButtons: boolean = true;
+  public ShowPersonN: boolean = true;
+  public ShowIcone: boolean = false;
+  public ShowForms: boolean = true;
+  public isDisabled: boolean = false;
+  public returnval: boolean = false;
+ 
+ constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
-
-  // get f() { return this.registrationForm.controls; }
-
   onSubmit() {
+
     this.isSubmitted = true;
-    if(!this.registrationForm.valid) {
+    if (!this.registrationForm.valid) {
       return false;
-    } 
-    
-    else {
-
-         this.itemList.push({ 'button': 'person' }); 
-         this.InpuValue1 = this.registrationForm.value.fields;
-         this.InpuValue2 = this.registrationForm.value.labels;
-
-         this.valueOf = this.InpuValue2.split(",");
-
-         for (var i = 0; i < this.InpuValue1; i++) {
-
-          this.itemList.push({ 'label': this.valueOf[i], 'fields': '' })
-        }
-        console.log(this.itemList);
-      }
     }
-  
-  OnkeyChange(){
-   
-   if(Number(this.registrationForm.value.labels))
-   { console.log('========daat');
-      this.registrationForm.controls.labels.reset();
-    //  this.Message =" Only String value is allowed"
-   }
-   else{}
-  }
+    else {
+      this.itemList.push({ 'button': 'person' });
+      this.InpuValue1 = this.registrationForm.value.fields;
+      this.InpuValue2 = this.registrationForm.value.labels;
+      this.arrdata = this.InpuValue2.split(",");
+      this.arrdataIndex = this.arrdata.forEach(element => {
+        let data = parseInt(element);
+       if (data) {this.returnval = true;}
+      });
+      if (this.returnval) return;
+      this.returnval = false;
+      for (var i = 0; i < this.InpuValue1; i++) {
+        this.itemList.push({ 'label': this.arrdata[i], 'fields': '' })
+      }
+      }
+    this.ShowForms = false;
+    if (this.registrationForm.value.radios == "female") {
 
-  addros(){
-    this.onSubmit();
+      this.ShowIcone = true;
+    }
+    else { this.ShowIcone = false; }
+    if (this.registrationForm.value.radios == "male") {
+      this.ShowPersonN = false;
+    }
+    else { this.ShowPersonN = true; }
+    
   }
-  
-  }  
+  ShowIcon() {
+    this.ShowIcone = false;
+  }
+  ShowButtons() {
+    this.EShowButtons = false;
+    this.SaveButton = true;
+    this.isDisabled = false;
+  }
+  SaveData(){
+    this.EShowButtons = true;
+    this.SaveButton = false;
+    this.isDisabled = true;
+  }
+  GetInputData()
+  {
+    if(this.registrationForm.value.fields >10)
+    {
+      this.registrationForm.controls.fields.reset();
+    }
+  }
+}
 
 
 
